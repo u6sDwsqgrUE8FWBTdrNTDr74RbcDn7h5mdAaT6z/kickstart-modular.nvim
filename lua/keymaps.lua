5083618b8ec891223acdf09568583be105e5c502
u6sDwsqgrUE8FWBTdrNTDr74RbcDn7h5mdAaT6z <68079809+u6sDwsqgrUE8FWBTdrNTDr74RbcDn7h5mdAaT6z@users.noreply.github.com>
2023-11-13 22:06:43 +0100

    Add tabline

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -28,4 +28,24 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Toggle filetree
 vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
 
+-- Buffer navigation
+vim.keymap.set('n', '<A-,>', '<Cmd>BufferPrevious<CR>')
+vim.keymap.set('n', '<A-.>', '<Cmd>BufferNext<CR>')
+
+vim.keymap.set('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>')
+vim.keymap.set('n', '<A->>', '<Cmd>BufferMoveNext<CR>')
+
+vim.keymap.set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>')
+vim.keymap.set('n', '<A-2>', '<Cmd>BufferGoto 2<CR>')
+vim.keymap.set('n', '<A-3>', '<Cmd>BufferGoto 3<CR>')
+vim.keymap.set('n', '<A-4>', '<Cmd>BufferGoto 4<CR>')
+vim.keymap.set('n', '<A-5>', '<Cmd>BufferGoto 5<CR>')
+vim.keymap.set('n', '<A-6>', '<Cmd>BufferGoto 6<CR>')
+vim.keymap.set('n', '<A-7>', '<Cmd>BufferGoto 7<CR>')
+vim.keymap.set('n', '<A-8>', '<Cmd>BufferGoto 8<CR>')
+vim.keymap.set('n', '<A-9>', '<Cmd>BufferGoto 9<CR>')
+vim.keymap.set('n', '<A-0>', '<Cmd>BufferLast<CR>')
+
+vim.keymap.set('n', '<A-x>', '<Cmd>BufferClose<CR>')
+
 -- vim: ts=2 sts=2 sw=2 et

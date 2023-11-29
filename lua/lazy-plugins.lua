7bdcf8a0cfa1f3cdba619b22471ece2743b7127a
u6sDwsqgrUE8FWBTdrNTDr74RbcDn7h5mdAaT6z <68079809+u6sDwsqgrUE8FWBTdrNTDr74RbcDn7h5mdAaT6z@users.noreply.github.com>
2023-11-29 19:17:35 +0100

    Reset git signs icons to default

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -55,14 +55,6 @@ require('lazy').setup({
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
       on_attach = function(bufnr)
         vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
 

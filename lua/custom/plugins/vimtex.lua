a2c6637a5144f960c4cef20d2bdd16bd5a71fcdb
u6sDwsqgrUE8FWBTdrNTDr74RbcDn7h5mdAaT6z <68079809+u6sDwsqgrUE8FWBTdrNTDr74RbcDn7h5mdAaT6z@users.noreply.github.com>
2024-02-14 21:27:48 +0100

    Add vimtex

---
+++ lua/custom/plugins/vimtex.lua
@@ -0,0 +1,9 @@
+return {
+  'lervag/vimtex',
+  lazy = false,
+  init = function()
+    vim.g.vimtex_view_method = 'zathura'
+    vim.g.vimtex_quickfix_mode = 0
+    vim.g.maplocalleader = ','
+  end
+}

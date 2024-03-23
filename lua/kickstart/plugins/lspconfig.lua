dc9676f839a823230bee22c8a0be9693ed492f21
u6sDwsqgrUE8FWBTdrNTDr74RbcDn7h5mdAaT6z <68079809+u6sDwsqgrUE8FWBTdrNTDr74RbcDn7h5mdAaT6z@users.noreply.github.com>
2024-03-23 16:05:20 +0100

    Add diagnostics icons

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -192,6 +192,13 @@ return {
           end,
         },
       }
+
+      -- Diagnostics icons
+      local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+      for type, icon in pairs(signs) do
+        local hl = 'DiagnosticSign' .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
     end,
   },
 }
